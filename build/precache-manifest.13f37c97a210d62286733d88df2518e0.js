8fd65ce75bce15850623937b416c0a1847693cb0
hanyaning <1193985098@qq.com>
2019-11-15 17:22:17 +0800

    message

---
+++ build/precache-manifest.13f37c97a210d62286733d88df2518e0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057c6ef1de87af34e007b676cbafbe6c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "797101306c837cedc235",
+    "url": "./static/css/2.6f60e7f3.chunk.css"
+  },
+  {
+    "revision": "a62046625bc47571333d",
+    "url": "./static/css/main.b67e0ded.chunk.css"
+  },
+  {
+    "revision": "797101306c837cedc235",
+    "url": "./static/js/2.5fb6f967.chunk.js"
+  },
+  {
+    "revision": "a62046625bc47571333d",
+    "url": "./static/js/main.b536b29e.chunk.js"
+  },
+  {
+    "revision": "be4014fb7f2baa9806ec",
+    "url": "./static/js/runtime-main.0f40977f.js"
+  },
+  {
+    "revision": "f6268db58ea1a7a5ffc6010bb41a10d8",
+    "url": "./static/media/bed_05.f6268db5.jpg"
+  },
+  {
+    "revision": "1ae576a3ba3c49bb1ff9b51baa4f521e",
+    "url": "./static/media/cup_05.1ae576a3.jpg"
+  },
+  {
+    "revision": "6e3c0e71538152a5f398d27ad92134d0",
+    "url": "./static/media/first_02.6e3c0e71.jpg"
+  },
+  {
+    "revision": "be26bb3c3f57237a2756d6eacbc9ef42",
+    "url": "./static/media/first_03.be26bb3c.jpg"
+  },
+  {
+    "revision": "e84416d68ca49ec2d51c53c84edc589b",
+    "url": "./static/media/first_06.e84416d6.jpg"
+  },
+  {
+    "revision": "567bad895f8fde93517a23090d898f72",
+    "url": "./static/media/first_08.567bad89.jpg"
+  },
+  {
+    "revision": "9d8b769123fc61dfaf4e376758f5f3ff",
+    "url": "./static/media/first_10.9d8b7691.jpg"
+  },
+  {
+    "revision": "9ab24f85f494c9720631775c51fe3252",
+    "url": "./static/media/first_15.9ab24f85.jpg"
+  },
+  {
+    "revision": "8aaf1911788208cbf84dcf9189c164f0",
+    "url": "./static/media/plant_05.8aaf1911.jpg"
+  },
+  {
+    "revision": "2e8ce08e92900e933d4d2c1aa229ab41",
+    "url": "./static/media/second_02.2e8ce08e.jpg"
+  },
+  {
+    "revision": "f233934e63ec72ef3e4250434c5a022b",
+    "url": "./static/media/smart_07.f233934e.jpg"
+  }
+]);
